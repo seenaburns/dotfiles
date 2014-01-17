64409888947114d98b4e5e1438721ea934355d80
Seena Burns <seenaburns@github>
2014-01-17 14:03:26 -0800

    Add general files, bash files, more mesasges to install.sh

--- install.sh
+++ install.sh
@@ -6,38 +6,46 @@
 
 # Color function
 # color_text message $GREEN|$RED
-GREEN="0;32m"
-RED="0;31m"
+GREEN="0;32m";
+RED="0;31m";
 function color_text () {
     # Print color
-    echo -ne  "\033[$2$1"
+    echo -ne  "\033[$2$1";
     
     # Reset attributes
     tput sgr0;
 }
-FAILED=$(echo -n "[ "; color_text FAILED $RED; echo -n " ]")
+SPACING="           ";
+FAILED=$(echo -n "[  "; color_text FAILED $RED; echo -n " ]");
+SKIP=$(echo -n "[    SKIP ]");
+INSTALL=$(echo -n "[ "; color_text INSTALL $GREEN; echo -n   " ]")
 
 # Custom install for bash_profile
 # Make existing bash_profile to bash_private
 if [ -f ~/.bash_profile ]
 then
-    echo ".bash_profile already exists, moving to .bash_private"
+    echo "$SPACING .bash_profile exists, attempting to move to .bash_private"
     if [ -f ~/.bash_private ]
     then
-        echo  "$FAILED .bash_private already exists, aborting"
+        echo  "$FAILED .bash_private exists, aborting"
         exit 1
     else
-        color_text "Moved .bash_profile to .bash_private"
+        "$SPACING Moved .bash_profile to .bash_private"
     fi
-    echo "Installing standard/.bash_profile to ~/"
+    echo "$INSTALL .bash_profile to ~/"
 fi
 
-# For other files 
+# For other files
+# Check existence, if not copy
 for f in $(ls -a standard/ | tail -n +3);
 do
-    echo $f;
+    # Skip .bash files (have separate installer above)
+    [ ${f:0:5} == ".bash" ] && continue
+
     if [ -f ~/$f ] || [ -d ~/$f ]
     then
-        echo "exists"
+        echo "$SKIP $f exists";
+    else
+        echo "$INSTALL $f"
     fi
 done;
