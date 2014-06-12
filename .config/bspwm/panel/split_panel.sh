2b6a1ec47baa42397d0724a57e55b8db126b0cb5
Seena Burns <hello@seenaburns.com>
2014-06-12 15:36:10 -0700

    bspwm: make panel span across windows, add padding

--- .config/bspwm/panel/split_panel.sh
+++ .config/bspwm/panel/split_panel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Split input by col (arg1) and pipe into dzen2 with offset (arg2)
+# Split input by col (arg1) and pipe into dzen2 with width (arg2) and offset (arg3)
 
 while read -r line;
 do
     echo $line | cut -d \: -f $1
-done | dzen2 -h 18 -w 200 -ta l -fn uushi:size=8 -x $2
+done | dzen2 -h 18 -w $2 -ta l -fn sans_serif:size=8 -x $3
