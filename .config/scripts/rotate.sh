7b71a3de5122f8cb6d84370ecafec273082d3c6d
Seena Burns <hello@seenaburns.com>
2014-10-13 13:24:16 -0700

    Fix rotate script for arch

--- .config/scripts/rotate.sh
+++ .config/scripts/rotate.sh
@@ -13,8 +13,9 @@ eval $(cat ~/.fehbg)
 # Restart panel, persist past closing shell
 while [[ -n $(pgrep panel) ]];
 do
-    pgrep panel | xargs kill 2>/dev/null;
+    pgrep panel | xargs kill -s 9;
 done
+
 nohup panel >/dev/null 2>&1 &
 nohup info_panel.sh >/dev/null 2>&1 &
 
