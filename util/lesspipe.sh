196f495045c2c9a7d8ca6fa5b3ae5a456831ce7f
Seena Burns <hello@seenaburns.com>
2017-08-20 11:55:59 -0700

    Add rust to lesspipe

--- util/lesspipe.sh
+++ util/lesspipe.sh
@@ -7,7 +7,7 @@ case "$1" in
    *.awk|*.groff|*.java|*.js|*.m4|*.php|*.pl|*.pm|*.pod|*.sh|\
    *.ad[asb]|*.asm|*.inc|*.[ch]|*.[ch]pp|*.[ch]xx|*.cc|*.hh|\
    *.lsp|*.l|*.pas|*.p|*.xml|*.xps|*.xsl|*.axp|*.ppd|*.pov|\
-   *.diff|*.patch|*.py|*.rb|*.sql|*.ebuild|*.eclass)
+   *.diff|*.patch|*.py|*.rb|*.sql|*.ebuild|*.eclass|*.rs)
       pygmentize "$1" ;;
    *) exit 0;;
 esac
