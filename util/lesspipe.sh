488cb80e5cbf3d913316011fac6a68c976513143
Seena Burns <hello@seenaburns.com>
2017-11-30 14:07:56 -0800

    [lesspipe] add more filetypes

--- util/lesspipe.sh
+++ util/lesspipe.sh
@@ -7,7 +7,9 @@ case "$1" in
    *.awk|*.groff|*.java|*.js|*.m4|*.php|*.pl|*.pm|*.pod|*.sh|\
    *.ad[asb]|*.asm|*.inc|*.[ch]|*.[ch]pp|*.[ch]xx|*.cc|*.hh|\
    *.lsp|*.l|*.pas|*.p|*.xml|*.xps|*.xsl|*.axp|*.ppd|*.pov|\
-   *.diff|*.patch|*.py|*.rb|*.sql|*.ebuild|*.eclass|*.rs)
+   *.diff|*.patch|*.py|*.rb|*.sql|*.ebuild|*.eclass|*.rs|*.json|*.go|*.pp)
       pygmentize "$1" ;;
+   *.bzl|*BUILD|*WORKSPACE) pygmentize -l python "$1";;
+   *.yaml) pygmentize -l yaml -fterminal16m -Ostyle=monokai "$1";;
    *) exit 0;;
 esac
