ee849d408ecc610b52cda8a74fd7b04207c3d7b5
Seena Burns <hello@seenaburns.com>
2018-04-02 15:30:31 +0000

    [emacs] ansitheme

---
+++ .emacs.d/themes/ansitheme.el
@@ -0,0 +1,20 @@
+;;; ansitheme.el
+;;; Commentary:
+;;; Code:
+
+(setq basic-colors-alist
+  '(("bg"      . nil)
+    ("fg"      . "white")
+    ("gray1"   . "brightblack")
+    ("gray2"   . "black")
+    ("white"   . "brightwhite")
+    ("red"     . "red")
+    ("green"   . "green")
+    ("yellow"  . "yellow")
+    ("blue"    . "blue")
+    ("magenta" . "magenta")
+    ("cyan"    . "cyan")
+    )
+)
+
+(provide 'ansitheme)
