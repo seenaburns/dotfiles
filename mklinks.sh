1022b89fb820979e1af694f76b80fca4c41d2583
Seena Burns <hello@seenaburns.com>
2020-10-05 08:45:24 -0700

    Remove mklinks ctags

--- mklinks.sh
+++ mklinks.sh
@@ -42,5 +42,4 @@ checkedlink .config/nvim/custom/functions.vim
 checkedlink .config/nvim/custom/customairline.vim
 checkedlink .config/nvim/syntax/todo.vim
 checkedlink .config/nvim/colors/nnkd.vim
-checkedlink .ctags
 
